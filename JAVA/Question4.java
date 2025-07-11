02a22965b4591085d69787c5c5303b20c0ffb5f8
NAVEEN <kolanaveen797@gmail.com>
2025-07-11 14:24:40 +0530

    added

--- JAVA/Question4.java
+++ JAVA/Question4.java
@@ -7,5 +7,27 @@ public class Question4 {
         int non_leap_year=year-leap_year;
         int total=(odd_days_leap+non_leap_year)%7;
         System.out.println(total);
+        switch (total) {
+            case 0:
+            System.out.println("mon");
+                break;
+            case 1:
+            System.out.println("tue");
+            break;
+            case 2:
+            System.out.println("wen");
+            break;
+            case 3:
+            System.out.println("thus");
+            break;
+            case 4:
+            System.out.println("fri");
+            break;
+            case 5:
+            System.out.println("sat");
+            break;
+            case 6:
+            System.out.println("sun");
+        }
         }
 }
