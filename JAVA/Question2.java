27a792d2add21ab31cda9cd435b9fe2e255e9cd5
NAVEEN <kolanaveen797@gmail.com>
2025-07-11 10:26:56 +0530

    question2

---
+++ JAVA/Question2.java
@@ -0,0 +1,30 @@
+public class Question2 {
+    public static void main(String[] args) {
+        int Seat_Number=8;
+        if (Seat_Number==1){
+            System.out.println("Lower");
+        }
+        else if (Seat_Number==2) {
+            System.out.println("Middle");
+        }
+        else if (Seat_Number==3){
+            System.out.println("Upper");
+        }
+        else if(Seat_Number==4){
+            System.out.println("Lower");
+        }
+        else if (Seat_Number==5){
+            System.out.println("Middle");
+        }
+        else if(Seat_Number==6){
+            System.out.println("Upper");
+        }
+        else if(Seat_Number==7){
+            System.out.println("Side Lower");
+        }
+        else{
+            System.out.print("Side Upper");
+        }
+        System.out.println((Seat_Number==1)?"Lower":(Seat_Number==2)?"Middle" : (Seat_Number==3)? "Upper": (Seat_Number==4)? "Lower" : (Seat_Number==5)? "Middle" :(Seat_Number==6)? "Upper" : (Seat_Number==7)?"Side Lower" :(Seat_Number==8)? "Side Upper" : "Invalid");
+    }
+}
